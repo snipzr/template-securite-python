a484a7ca05a72495e026c201f5f11cd4257b0dca
snipzr <adn.meslin@gmail.com>
2026-04-05 12:59:18 +0200

    payload pour la soumission post

--- src/tp3/utils/session.py
+++ src/tp3/utils/session.py
@@ -1,28 +1,18 @@
 import requests
+from src.tp3.utils.config import logger
 from src.tp3.utils.captcha import Captcha
 
 class Session:
     """
     Class representing a session to solve a captcha and submit a flag.
-
-    Attributes:
-        url (str): The URL of the captcha.
-        captcha_value (str): The value of the solved captcha.
-        flag_value (str): The value of the flag to submit.
-        valid_flag (str): The valid flag obtained after processing the response.
     """
 
     def __init__(self, url):
-        """
-        Initializes a new session with the given URL.
-
-        Args:
-            url (str): The URL of the captcha.
-        """
         self.url = url
         self.captcha_value = ""
-        self.flag_value = ""
-        self.valid_flag = ""
+        self.flag_guess = 1000
+        self.final_flag_buffer = ""
+        self.response_bucket = ""
         
         self.http_sess = requests.Session()
 
@@ -30,17 +20,25 @@ class Session:
         """
         Prepares the request for sending by capturing and solving the captcha.
         """
-        captcha = Captcha(self.url)
+        captcha = Captcha(self.url, self.http_sess)
         captcha.capture()
         captcha.solve()
 
         self.captcha_value = captcha.get_value()
-        self.flag_value = "FIXME"
+        logger.info(f"captcha lu : {self.captcha_value} | flag tester : {self.flag_guess}")
 
     def submit_request(self):
         """
         Sends the flag and captcha.
         """
+        current_try_payload = {
+            "flag": str(self.flag_guess),
+            "captcha": self.captcha_value,
+            "submit": "Envoyer",
+        }
+
+        resp = self.http_sess.post(self.url, data=current_try_payload)
+        self.response_bucket = resp.text
 
     def process_response(self):
         """
