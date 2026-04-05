be6928cbba3a86d745f7332cf137e304eb51ef15
snipzr <adn.meslin@gmail.com>
2026-04-05 14:31:18 +0200

    clean du main pour garder un flow simple

--- src/tp3/main.py
+++ src/tp3/main.py
@@ -1,26 +1,27 @@
 from src.tp3.utils.config import logger
 from src.tp3.utils.session import Session
 
-
 def main():
-    logger.info("Starting TP3")
+    logger.info("Debut TP3 - Brute force Captcha")
 
-    ip = "31.220.95.27:9002"
-    challenges = {"1": f"http://{ip}/captcha1/"}
+    ip_target = "31.220.95.27:9002"
+    challenge_list = {"1": f"http://{ip_target}/captcha1/"}
 
-    for i in challenges:
-        url = challenges[i]
-        session = Session(url)
-        session.prepare_request()
-        session.submit_request()
 
-        while not session.process_response():
-            session.prepare_request()
-            session.submit_request()
+
+    for idx in challenge_list:
+        target_url = challenge_list[idx]
+        logger.info(f"--> Target {idx} : {target_url}")
+        sess = Session(target_url)
+        sess.prepare_request()
+        sess.submit_request()
+
+        while not sess.process_response():
+            sess.prepare_request()
+            sess.submit_request()
 
         logger.info("Smell good !")
-        logger.info(f"Flag for {url} : {session.get_flag()}")
-
+        logger.info(f"Le flag de {target_url} est : {sess.get_flag()}")
 
 if __name__ == "__main__":
     main()
